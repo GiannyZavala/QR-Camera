7c9fc640a441aa0137f7ce00b4f8c5ce4b372325
Gianny Rivera <giedriveraza@ittepic.edu.mx>
2017-11-16 16:16:58 -0700

    feat: Se creo boton el escanner

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BarcodeScanner,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  options: BarcodeScannerOptions;
+  results: {};
 
-  constructor(public navCtrl: NavController) {
+  constructor(private barcode: BarcodeScanner,public navCtrl: NavController) {
 
   }
+   
+  async scanBarcode(){
+    this.options={
+      prompt:'Escanea el código QR para guardar contacto'
+    }
+    this.results= await this.barcode.scan(this.options);
+    console.log(this.results);
+  }
 
+ 
 }
